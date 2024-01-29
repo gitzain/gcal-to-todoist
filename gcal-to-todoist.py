b95e1e3684ed098add2535de440742fed89e0356
Zain Khan <emailzainkhan@gmail.com>
2024-01-29 04:04:12 +0000

    Using github secrets

--- gcal-to-todoist.py
+++ gcal-to-todoist.py
@@ -3,18 +3,19 @@ import os
 from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
 from todoist_api_python.api import TodoistAPI  # Import for todoist-api-python
+import base64
+import json
 
 # Environment variables
+GOOGLE_SERVICE_KEY = os.environ.get('GOOGLE_SERVICE_KEY')
 GOOGLE_CALENDAR_ID = os.environ.get('GOOGLE_CALENDAR_ID')
 TODOIST_API_TOKEN = os.environ.get('TODOIST_API_TOKEN')
 TODOIST_REFRESH_TOKEN = os.environ.get('TODOIST_REFRESH_TOKEN')
-TODOIST_PROJECT_ID = int(os.environ.get('TODOIST_PROJECT_ID'))
+TODOIST_PROJECT_ID = os.environ.get('TODOIST_PROJECT_ID')
 
 # Google Calendar API setup
-credentials = Credentials.from_service_account_file(
-    'key.json',
-    scopes=['https://www.googleapis.com/auth/calendar.readonly']
-)
+google_service_key_contents = GOOGLE_SERVICE_KEY
+credentials = Credentials.from_service_account_info(json.loads(google_service_key_contents), scopes=['https://www.googleapis.com/auth/calendar.readonly'])
 service = build('calendar', 'v3', credentials=credentials)
 
 # Today's date and time ranges
@@ -50,7 +51,7 @@ else:
 
             already_exists = False
             for current_task in tasks:
-                if current_task.content == summary:
+                if current_task.content == summary and current_task.due.datetime == due_date:
                     already_exists = True
 
             
@@ -60,6 +61,5 @@ else:
                     project_id=TODOIST_PROJECT_ID,
                     due_datetime=due_date if due_date else None
                 )
-                print(str(task))
         except Exception as error:
             print(error)
